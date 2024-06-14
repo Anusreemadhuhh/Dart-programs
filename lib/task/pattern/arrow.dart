e90db3648297577423df0064c8ac90666ff22bf1
anusree <anusreemadhu04@gmail.com>
2024-06-14 10:01:28 +0530

    first

--- lib/task/pattern/arrow.dart
+++ lib/task/pattern/arrow.dart
@@ -3,29 +3,30 @@ void main()
 {
   double i,j,k;
   print("Enter the rows : ");
-  double rows = double.parse(stdin.readLineSync()!);
-  for (i = 1; i < rows; i++)
+  double row = double.parse(stdin.readLineSync()!);
+  for(i=1;i<=row;i++)
   {
-    for (j = 0; j<i; j++)
+    for(j=1;j<=(row-i);j++)
     {
       stdout.write(" ");
     }
-    for (k = 0; k <rows+1; k++)
+    for(k=1;k<=row+1;k++)
     {
       stdout.write("*");
     }
     stdout.write("\n");
   }
-  for (i = 0; i < rows; ++i)
+  for(i=2;i<=row;i++)
   {
-    for (j = 0; j < rows-i; j++)
+    for(j=2;j<=i;j++)
     {
       stdout.write(" ");
     }
-    for (k = 0; k < rows+1; k++)
+    for(k=1;k<=row+1;k++)
     {
       stdout.write("*");
     }
     stdout.write("\n");
   }
+
 }
